c82f55926678a2da58007264f33bb234825796e1
Ivan Kyvatskyi <ikyvatskyi@spendhq.com>
2022-11-27 19:46:01 +0200

    Added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'rules': {
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single']
+    }
+};
